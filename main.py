1154826ba823ebd4c066b0e15bd9ee868f672232
Арипов Никита <aripovn@gmail.com>
2024-01-04 12:01:14 +0300

    get info about subsystems

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import secret
+import re
 
 
 class ObjectTree:
@@ -8,16 +9,44 @@ class ObjectTree:
 
 
 def build_object_tree():
-    path = secret.path_to_configuration()
-    configuration = 'src/Configuration/Configuration.mdo'
-    conf_file = path + configuration
-
-    with open(conf_file, 'r') as f:
-        print(f.readlines())
 
     obj = ObjectTree('ДемонстрационноеПриложение')
     print(obj.configuration_name)
 
+    path = secret.path_to_configuration()
+    configuration = 'src/Configuration/Configuration.mdo'
+    subsystem_path = 'src/Subsystems/'
+
+    conf_file = path + configuration
+
+    reg_exp_pattern_subsystem = '(?<=<subsystems>Subsystem.).*?(?=</subsystems>)'
+    reg_exp_pattern_content = '(?<=<content>).*?(?=</content>)'
+
+    # get upper subsystems
+    upper_subsystems = []
+    with open(conf_file, 'r') as f:
+        for line in f.readlines():
+            m = re.search(reg_exp_pattern_subsystem, line)
+            if not(m is None):
+                upper_subsystems.append(m.group())
+
+    for subsystem in upper_subsystems:
+        contents = []
+        subsystems = []
+        subsystem_name = path + subsystem_path + subsystem + '/' + subsystem + '.mdo'
+        with open(subsystem_name, 'r') as f:
+            for line in f.readlines():
+                m = re.search(reg_exp_pattern_subsystem, line)
+                if not (m is None):
+                    subsystems.append(m.group())
+
+                m = re.search(reg_exp_pattern_content, line)
+                if not (m is None):
+                    contents.append(m.group())
+        info = {'subsystems': subsystems, 'contents': contents}
+        obj.subsystems.append({subsystem: info})
+
+    print(obj.subsystems)
 
 
 if __name__ == '__main__':

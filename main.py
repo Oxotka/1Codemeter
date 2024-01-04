de0bb2f41f2bb4fa8f48ab51ade37be0a94818ef
Арипов Никита <aripovn@gmail.com>
2024-01-04 12:26:52 +0300

    add info about inner subsystems

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import secret
 import re
+import os
 
 
 class ObjectTree:
@@ -11,43 +12,51 @@ class ObjectTree:
 def build_object_tree():
 
     obj = ObjectTree('ДемонстрационноеПриложение')
-    print(obj.configuration_name)
-
     path = secret.path_to_configuration()
     configuration = 'src/Configuration/Configuration.mdo'
     subsystem_path = 'src/Subsystems/'
-
-    conf_file = path + configuration
 
     reg_exp_pattern_subsystem = '(?<=<subsystems>Subsystem.).*?(?=</subsystems>)'
     reg_exp_pattern_content = '(?<=<content>).*?(?=</content>)'
 
     # get upper subsystems
     upper_subsystems = []
-    with open(conf_file, 'r') as f:
+    with open(path + configuration, 'r') as f:
         for line in f.readlines():
             m = re.search(reg_exp_pattern_subsystem, line)
             if not(m is None):
                 upper_subsystems.append(m.group())
 
+    # get info about subsystems
     for subsystem in upper_subsystems:
-        contents = []
-        subsystems = []
-        subsystem_name = path + subsystem_path + subsystem + '/' + subsystem + '.mdo'
-        with open(subsystem_name, 'r') as f:
-            for line in f.readlines():
-                m = re.search(reg_exp_pattern_subsystem, line)
-                if not (m is None):
-                    subsystems.append(m.group())
+        path_to_dir_subsystem = path + subsystem_path + subsystem + '/'
+        info_subsystem = info_about_subsystems(subsystem, path_to_dir_subsystem, reg_exp_pattern_content)
+        obj.subsystems.append(info_subsystem)
 
-                m = re.search(reg_exp_pattern_content, line)
-                if not (m is None):
-                    contents.append(m.group())
-        info = {'subsystems': subsystems, 'contents': contents}
-        obj.subsystems.append({subsystem: info})
+    return obj
 
-    print(obj.subsystems)
+
+def info_about_subsystems(subsystem, path, reg_exp_pattern_content):
+    subsystem_name = path + subsystem + '.mdo'
+    contents = []
+    subsystems = []
+    if os.path.isdir(path + 'Subsystems'):
+        inner_subsystems = [f for f in os.listdir(path + 'Subsystems') if f != '.DS_Store']
+        for inner_subsystem in inner_subsystems:
+            inner_subsystem_path = path + 'Subsystems/' + inner_subsystem + '/'
+            inner_info = info_about_subsystems(inner_subsystem, inner_subsystem_path, reg_exp_pattern_content)
+            subsystems.append({inner_subsystem: inner_info})
+
+    with open(subsystem_name, 'r') as f:
+        for line in f.readlines():
+            # contents
+            m = re.search(reg_exp_pattern_content, line)
+            if not (m is None):
+                contents.append(m.group())
+
+    return {subsystem: {'subsystems': subsystems, 'contents': contents}}
 
 
 if __name__ == '__main__':
-    build_object_tree()
+    obj = build_object_tree()
+    print(obj.subsystems)
